766dbe7e40eba9a6dec37fc241b004b1973f22e6
prabodhaharankahadeniya <prabodhanh@gmail.com>
2017-07-20 16:34:23 +0530

    view items in the list view

--- app/src/main/java/com/example/prabodhaharankahadeniya/persistence/ViewUsersActivity.java
+++ app/src/main/java/com/example/prabodhaharankahadeniya/persistence/ViewUsersActivity.java
@@ -2,14 +2,23 @@ package com.example.prabodhaharankahadeniya.persistence;
 
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
 @ContentView(R.layout.activity_view_users)
 public class ViewUsersActivity extends RoboActivity {
 
 
+    private static final String TAG="ViewUserActivity";
     private DBHandler dbHandler;
     public  DBHandler getDBHandler(){
         if(dbHandler==null){
@@ -18,12 +27,82 @@ public class ViewUsersActivity extends RoboActivity {
         return dbHandler;
     }
 
-    User[] users;
+    private User[] users;
+    @InjectView(R.id.list_view) private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         users=getDBHandler().getAllUser();
+        Log.d(TAG," "+users.length);
+        listView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return users.length;
+            }
 
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int i, View convertView, ViewGroup viewGroup) {
+                User user=users[i];
+                View cellUser=null;
+                if(convertView==null){
+                    cellUser= LayoutInflater.from(ViewUsersActivity.this).inflate(R.layout.user_list_item,null);
+
+                }
+                else {
+                    cellUser=convertView;
+                }
+
+                PlaceHolder ph=(PlaceHolder)cellUser.getTag();
+                TextView nameTV;
+                TextView ageTV;
+                TextView cityTV;
+
+                if(ph==null){
+                    nameTV=cellUser.findViewById(R.id.name);
+                    ageTV= cellUser.findViewById(R.id.age);
+                    cityTV=cellUser.findViewById(R.id.city);
+
+
+                    ph=new PlaceHolder();
+                    ph.name=nameTV;
+                    ph.age=ageTV;
+                    ph.city=cityTV;
+                    cellUser.setTag(ph);
+
+                }
+                else {
+                    nameTV=ph.name;
+                    ageTV=ph.age;
+                    cityTV=ph.city;
+
+                }
+
+
+                nameTV.setText(user.getName());
+                ageTV.setText(user.getAge());
+                cityTV.setText(user.getCity());
+
+
+                return cellUser;
+            }
+        });
+
+    }
+
+    private class PlaceHolder{
+        TextView name;
+        TextView age;
+        TextView city;
     }
 }
